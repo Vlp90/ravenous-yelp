cbfe0f2e5993c4452c272103b6eebda3e3f4047f
Vlp90 <vladimir.leloup@gmail.com>
2020-11-06 11:10:40 +0100

    exercice done

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -5,7 +5,9 @@ import SearchBar from '../SearchBar/SearchBar'
 
 function App() {
   return (
-    <div className="app">
+    <div className="App">
+      <h1>ravenous</h1>
+
           <SearchBar />
         <BusinessList />
     </div>
